1e359bf847258688acb15343d40f8adff4f961c0
Eduardo Morales <emoral435@gmail.com>
2022-12-06 20:16:35 -0600

    Started menu functionality

--- src/modules/contact-page.js
+++ src/modules/contact-page.js
@@ -1,7 +1,40 @@
 import { makeDiv } from "./initial-page";
 
 export default function contactPage() {
+    // making the contact div that gets appended to the main content section of the page
     let contactDiv = makeDiv("contactSection");
+    // making the phone number section that provides random phone numbers
+    let phoneNum = makeDiv('phoneNum')
+    let phoneHeader = makeDiv('phoneHeader')
+    let phoneTitle = makeDiv('phoneTitle')
+    phoneTitle.textContent = 'Phone Numbers'
+    let phoneImg = new Image(150, 120);
+    phoneImg.src = '/src/images/phone.png'
+    phoneImg.classList.add('phoneImg')
+    phoneHeader.append(phoneTitle, phoneImg);
+    phoneNum.append(phoneHeader);
+    let phoneList = makeDiv('phoneList')
+    let phoneArray = ['+(847) 833-3438', '+(775) 837-2384', '+(778) 330-2389', "+(123) 456-7890", 'Feel Free To Call!']
+    for (let i = 0; i < phoneArray.length; i++) {
+        let phoneNumber = makeDiv('phoneNumber')
+        phoneNumber.textContent = phoneArray[i]
+        phoneList.appendChild(phoneNumber)
+    } phoneNum.append(phoneList)
+    // making the address section that provides random addresses
+    let address = makeDiv('address')
+    let addressHeader = makeDiv('addressHeader')
+    let addressTitle = makeDiv('addressTitle')
+    addressTitle.textContent = 'Addresses'
+    let addressImg = new Image(200, 110)
+    addressImg.classList.add('addressImg')
+    addressImg.src = '/src/images/map.png'
+    addressHeader.append(addressImg, addressTitle)
+    address.append(addressHeader)
+    let announced = makeDiv('Announced')
+    announced.textContent = 'To Be Announced!'
+    address.append(announced)
+    // final appending to the contact div
+    contactDiv.append(phoneNum, address)
     return contactDiv;
 }
 
--- src/modules/menu-page.js
+++ src/modules/menu-page.js
@@ -3,5 +3,25 @@ import { makeDiv } from "./initial-page";
 export default function menuPage() {
     // make the main menuPage div
     let menuDiv = makeDiv('menuSection');
+    // define the six pictures
+    let deoxys = new Image(300, 300)
+    deoxys.src = '/src/images/deoxys.png'
+    let sushi = new Image(300, 270)
+    sushi.src = '/src/images/sushi.png'
+    let pizza = new Image(300, 300)
+    pizza.src = '/src/images/pizza.png'
+    let squid = new Image(280, 300)
+    squid.src = '/src/images/squidward.png'
+    let mcdy = new Image(300, 285)
+    mcdy.src = '/src/images/donaldmcdonald.png'
+    let drink = new Image(156, 300)
+    drink.src = '/src/images/drink.png'
+    let picArr = [sushi, mcdy, drink, pizza, squid, deoxys]
+    for (let i =0; i < picArr.length; i++) {
+        let background = makeDiv('background')
+        background.dataset.id = toString(i)
+        background.append(picArr[i])
+        menuDiv.append(background)
+    }
     return menuDiv;
 }
